585dd7c3aac8cf8541740433d0273c153e2067bb
jorgegoco <jorgegoco70@gmail.com>
2023-09-14 09:36:10 +0200

    Change data, and finish styling

--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -10,17 +10,17 @@ export type ProductType = {
 const initState: ProductType[] = [
     {
         "sku": "item0001",
-        "name": "Bonsai",
+        "name": "Widget",
         "price": 9.99
     },
     {
         "sku": "item0002",
-        "name": "Roses",
+        "name": "Premium Widget",
         "price": 19.99
     },
     {
         "sku": "item0003",
-        "name": "Orchid",
+        "name": "Deluxe Widget",
         "price": 29.99
     }
 ]
@@ -34,7 +34,7 @@ const ProductsContext = createContext<UseProductsContextType>(initContextState)
 type ChildrenType = { children?: ReactElement | ReactElement[] }
 
 export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
-    const [products, setProducts] = useState<ProductType[]>(initState)
+    const [products] = useState<ProductType[]>(initState)
 
     // useEffect(() => {
     //     const fetchProducts = async (): Promise<ProductType[]> => {
